0f2cae799902b92f1cbd4e81cb764fc233bc6a99
Arnaud Endignous <94105105+Piryth@users.noreply.github.com>
2024-01-03 17:59:57 +0000

    added sources

---
+++ src/main/java/com/example/huffmanassignment/HuffmanAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.huffmanassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuffmanAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuffmanAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/huffmanassignment/HuffmanAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.huffmanassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuffmanAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
